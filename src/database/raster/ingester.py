ed876083022e7a725753125bf6b011d03ab25250
chris010970 <c.r.williams0109@gmail.com>
2020-09-17 22:56:44 +0100

    Reorganised raster ingestion code

--- src/database/raster/ingester.py
+++ src/database/raster/ingester.py
@@ -2,11 +2,13 @@ import os
 import copy
 import asyncio
 import tempfile
+import argparse
 
 from threading import Thread
 from progressbar import ProgressBar
-
+ 
 from src.utility import parser
+from src.database.objects.manager import Manager
 
 
 class Ingester:
@@ -217,3 +219,48 @@ class Ingester:
             return True
 
         return False
+
+
+def parseArguments(args=None):
+
+    """
+    parse command line arguments
+    """
+
+    # parse command line arguments
+    parser = argparse.ArgumentParser(description='process-ard')
+    parser.add_argument('config_file', action="store") # 'C:\\Users\\Chris.Williams\Desktop\\ingest.yml'
+    parser.add_argument('repository', action="store")
+    parser.add_argument('product', action="store")
+
+    return parser.parse_args(args)
+
+
+def main():
+
+    """
+    main path of execution
+    """
+
+    # parse arguments
+    args = parseArguments()
+    manager = Manager( args.config_file )
+
+    # get repository
+    repo = manager.getRepository( args.repository )
+    if repo is not None:
+
+        # get product
+        product = repo.getProduct( args.product )
+        if product is not None:
+
+            # ingest images
+            obj = Ingester( repo )
+            obj.process ( product )
+            
+    return
+    
+
+# execute main
+if __name__ == '__main__':
+    main()
