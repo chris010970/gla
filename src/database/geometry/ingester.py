93ee00688cdde30551c8e1c64e03d38d534a1be6
chris010970 <c.r.williams0109@gmail.com>
2020-09-17 22:52:36 +0100

    Updated geometry ingester

--- src/database/geometry/ingester.py
+++ src/database/geometry/ingester.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import tempfile
+import argparse
 
 import src.utility.ogr2ogr as ogr2ogr
 from src.database.objects.server import Server
@@ -97,3 +98,46 @@ class Ingester:
 
         # execute ogr2ogr
         return ogr2ogr.main( cmd_args )
+
+
+def parseArguments(args=None):
+
+    """
+    parse command line arguments
+    """
+
+    # parse command line arguments
+    parser = argparse.ArgumentParser(description='process-aoi')
+    parser.add_argument('geometry_file', action="store")
+    parser.add_argument('config_file', action="store")
+
+    # naming options
+    parser.add_argument('-schema', default='aoi', action="store" )
+    parser.add_argument('-table', default=None, action="store" )
+
+    return parser.parse_args(args)
+
+
+def main():
+
+    """
+    main path of execution
+    """
+
+    # parse arguments and validate
+    args = parseArguments()
+    if os.path.exists( args.geometry_file ) and os.path.exists( args.config_file ):
+
+        # setup defaults
+        if args.table is None:
+            args.table = os.path.splitext( os.path.basename( args.geometry_file ) )[ 0 ]
+
+        # create instance and execute ingestion    
+        obj = Ingester( args.config_file )
+        obj.process( args )
+
+    return
+    
+# execute main
+if __name__ == '__main__':
+    main()
